aa58e62e1b380279fda27e9baffe2fae34b25035
Diego Hermys <diegohermys@gmail.com>
2022-07-24 14:43:10 -0300

    commit inicial

---
+++ src/main/java/br/com/alura/linguagemapi/LinguagemapiApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.linguagemapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinguagemapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinguagemapiApplication.class, args);
+	}
+
+}
